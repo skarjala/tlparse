8da3f3c014cb9302e2a2290ae86f3f2e00f4976d
Sandeep Narendranath Karjala <skarjala@meta.com>
2025-08-21 13:23:20 -0700

    fix template

--- src/templates.rs
+++ src/templates.rs
@@ -185,6 +185,9 @@ PT2 generates <a href='chromium_events.json'>Chromium Trace Events</a> in JSON o
 You can download and view them in a tool like <a href='https://ui.perfetto.dev/'>Perfetto</a>.
 {{ endif  }}
 <p>
+<a href="collectives_parity.json">NCCL collectives parity report</a> comparing scheduler and generated code operations.
+</p>
+<p>
 Build products below:
 </p>
 <ul>
